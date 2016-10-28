b9bcafaa212300a089ba213ea644633f2c468874
Javier Soto <javiers@twitter.com>
2016-10-28 10:43:09 -0700

    Initial Commit

---
+++ RubikSwift/RubikSwift.h
@@ -0,0 +1,19 @@
+//
+//  RubikSwift.h
+//  RubikSwift
+//
+//  Created by Javier Soto on 10/28/16.
+//  Copyright © 2016 Javier Soto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RubikSwift.
+FOUNDATION_EXPORT double RubikSwiftVersionNumber;
+
+//! Project version string for RubikSwift.
+FOUNDATION_EXPORT const unsigned char RubikSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RubikSwift/PublicHeader.h>
+
+
